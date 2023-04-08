2fe26fa6441d845cd8aca7f7951e62d057ad881c
Raphael Shodi <raphael.s.k@hotmail.com>
2023-04-08 20:26:15 -0300

    ajeitado

--- main.rb
+++ main.rb
@@ -1,20 +1,19 @@
-frase = "Agendar com José reunião às 10:00 #trabalho hoje"
-hora = frase.match(/(([0-9][0-9]?[\:|\ ]([0-9]{2}|[a-z]{4}[a-z]?))|([a-zà-ú]{2}\ [0-9]{2}))/)
+#frase = "Agendar com José reunião às 10:00 #trabalho hoje"
+puts "Digite a frase: "
+frase = gets
+hora = frase.match(/((às)\s[0-9]+)|([0-9]+(\s(([0-9]+)|horas?)|\:[0-9]+))/)
 
-
-acao = frase.match(/agendar|marcar|reunião/)
-
+acao = frase.match(/[A-z](gendar|arcar|eunião)/)
 
 data = frase.match(/((ante|depois)\ (de\ )?)?(amanhã|ontem|hoje)|[0-9]{2}(\/[0-9]{2}(\/[0-9]{4})?|\ (de\ )?[A-z](aneiro|evereiro|arço|bril|aio|unho|ulho|gosto|etembro|utubro|ovembro|ezembro)(\ de)?(\ [0-9]{4})?)/)
 
-
-
 name = frase.match(/com\ [A-Z][a-zà-ú]+(\ e\ [A-Z][a-zà-ú]+)?/)
 nomes = name.to_s.scan(/[A-Z][a-zà-ú]+/)
+
 tags = frase.match(/#[A-z]+/)
 
 
-puts " Data: #{data}"
+puts "Data: #{data}"
 puts "hora: #{hora}"
 puts "nome(s): #{nomes.inspect}"
 puts "Ação: #{acao}"
